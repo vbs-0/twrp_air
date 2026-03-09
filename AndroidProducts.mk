9db08f6e30020f1ec6b4b463859b7fd0f151319a
vbs-0 <chbhadri0@gmail.com>
2026-03-09 09:49:45 +0530

    Fix KeyMint linker error: replace V1 with V3 libs

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+# Copyright (C) 2017-2023 The Android Open Source Project
+# Copyright (C) 2014-2023 The Team Win LLC
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_air.mk
+
+COMMON_LUNCH_CHOICES := twrp_air-ap2a-eng
